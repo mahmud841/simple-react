f13387d943dedbed5ba5288b0c307b32f4b6719c
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-17 18:46:16 +0800

    first commit

---
+++ countries.js
@@ -0,0 +1,34 @@
+const loadCountries = () => {
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(res => res.json())
+  .then(data  => displayCountries(data))
+}
+
+const displayCountries = countries => {
+  console.log(countries[0]);
+  const container = document.getElementById('countries');
+  const countryHTML = countries.map(country => getCountryHTML (country))
+  container.innerHTML= countryHTML.join(' ');
+  console.log(countryHTML[0]);
+};
+/* const getCountryHTML = country => {
+  return `
+        <div class = "country"> 
+        <h2>${country.name} </h2>
+        <h4> capital: ${country.capital}</h4>
+        <img src = "${country.flag}">
+        </div>
+  `
+} */
+const getCountryHTML = country => {
+  const {name, capital, flag} = country;
+  return `
+        <div class = "country"> 
+        <h2>${name} </h2>
+        <h4> capital: ${capital}</h4>
+        <img src = "${flag}">
+        </div>
+  `
+}
+
+loadCountries();
